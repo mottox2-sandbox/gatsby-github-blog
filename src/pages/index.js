dcd825cc7559d6cf1a28c0254ddd9b96ebab079f
mottox2 <y.mottox2@gmail.com>
2019-01-05 04:15:59 +0900

    Add issues to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = (props) => {
+  return <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    { props.data.github.repository.issues.edges.map(edge => {
+      return <div>
+        <h2>{edge.node.title}</h2>
+        <p dangerouslySetInnerHTML={{ __html: edge.node.bodyHTML }} />
+      </div>
+    })}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+}
+
+export const query = graphql`
+{
+  github {
+    repository(owner: "mottox2-sandbox", name: "gatsby-github-blog") {
+      issues(first: 10) {
+        edges {
+          node {
+            id
+            bodyHTML
+            title
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
